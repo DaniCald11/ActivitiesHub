8429307ca41217d053a1aa7a3ab43709f346206b
Olga Calderon Amezcua <143193423+DaniCald11@users.noreply.github.com>
2026-09-08 18:10:56 -0600

    Parcial 01 - Add API Skeleton (#1)
    * feat(parcial-01) - add initial boilerplate
    
    * feat(parcial-01) - cleanup boilerplate
    
    * feat(parcial01) - add entity and EF boilerplate
    
    * feat(parcial01) - add first entity and initial migration
    
    * feat(parcial01) - add db initializer
    
    * feat(parcial01) - rename sln, csproj and folders
    
    * feat(parcial01) - add events controllers
    
    * feat(parcial01) - add bruno integration tests
    
    * feat(parcial01) - update bruno integration tests
    
    * feat(parcial01) - add openapi including rpc clients generating
    
    * feat(parcial01) - add react app skeleton
    
    * feat(parcial01) - update react app skeleton

---
+++ src/EventsHub.Api/Controllers/EventsHubBaseController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+namespace EventsHub.Api.Controllers;
+
+[Route("api/v1/[controller]")] //ruta base
+[ApiController] 
+
+public class EventsHubBaseController : ControllerBase
+{
+}
---
+++ web/EventsHub/vite.config.ts
@@ -0,0 +1,15 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
